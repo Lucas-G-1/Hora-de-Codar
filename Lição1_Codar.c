f27df8c8d911dd64f7a326faf03266759d443041
Lucas-G-1 <122480830+Lucas-G-1@users.noreply.github.com>
2026-02-04 20:29:27 +0000

    Criação do codigo basico com o if

---
+++ Lição1_Codar.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+    float temperatura, umidade;
+    unsigned int estoque, estoqueMinimo = 1000;
+
+    printf("Entre com a temperatura: \n");
+    scanf("%f",&temperatura);
+    printf("Entre com umidade: \n");
+    scanf("%f", &umidade);
+    printf("Entre com estoque: \n");
+    scanf("%u", &estoque);
+
+    if ( temperatura > 30){
+        printf("Temperatura está muito alta\n");
+    } else{
+        printf("Temperatura está dentro dos parametros.\n");
+    }
+
+    if (umidade > 50){
+        printf("Umidade elevada. \n");
+    } else {
+        printf ("Umidade está dentro dos parametros.\n");
+    }
+
+    if (estoque < estoqueMinimo){
+        printf("Estoque abaixo do minimo.\n");
+    } else {
+        printf("Estoque normal.\n");
+    }
+
+}
