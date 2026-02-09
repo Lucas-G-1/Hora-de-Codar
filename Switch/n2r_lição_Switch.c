6fba366d65a21794f0ca93a2ef83f9f65a1e0be2
Lucas-G-1 <122480830+Lucas-G-1@users.noreply.github.com>
2026-02-09 21:42:43 +0000

    usa a introdução a variavel Switch

---
+++ Switch/n2r_lição_Switch.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+    int variavel;
+    printf("Digite um valor\n");
+    scanf("%d",&variavel);
+    switch (variavel) {
+  case 1:
+    printf("Código a ser executado se variavel == 1\n");
+    break;
+  case 2:
+    printf("Código a ser executado se variavel == 2\n");
+    break;
+  default:
+    printf("Código a ser executado se a variavel não for nem 1 ou 2\n");
+}
+
+}
