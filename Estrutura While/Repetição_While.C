e093be7b9eb5526953dabff66a0f7a898e96a95f
Lucas-G-1 <122480830+Lucas-G-1@users.noreply.github.com>
2026-02-17 20:41:49 +0000

    Criação de exemplo pratico da estrutura While

---
+++ Estrutura While/Repetição_While.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int i = 1; //variavel de incremento
+
+    while (i <= 15) //variavel de incremento
+    {
+        printf("%d\n", i); //saida
+        i++; //incremento
+    }
+    
+}
